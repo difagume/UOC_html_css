576256e50957af61fe97ecd43fc463eb68566807
Diego Fabricio <difagume@gmail.com>
2020-04-04 00:39:22 -0500

    función para cargar imagenes en modal

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const name = '😊'
 console.log(`Hi ${name}`)
 
+// Scroll top
 window.onscroll = function () {
   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30)
     document.getElementById('top').style.display = 'block';
   else
     document.getElementById('top').style.display = 'none';
 };
+
+// Evento para mostrar imagen en dialogo
+$(document).on('click', '.openImgDialog', function () {
+  const imgPath = $(this).data('id');
+  console.log(`Obteniendo path de img ${imgPath} para mostrar en modal`);
+  $('.modal-body #imageModal').attr('src', imgPath);
+});
