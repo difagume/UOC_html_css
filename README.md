0771c94ba03b1e4f43bf1e6b4b3d92ce94832274
Diego Fabricio <difagume@gmail.com>
2020-04-03 20:54:14 -0500

    Agregado secciones: index, empezando, inicioRapido, preparar, css

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# UOC_html_css
-Repositorio de materia herramietnas html y css del 2do semestre de la UOC
+# UOC_html_css_Pec1
+Repositorio de html y css del 2do semestre de la UOC, Pec 1 Desarrollo de una web
--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const name = 'world'
-console.log(`Hello ${name}`)
+const name = '😊'
+console.log(`Hi ${name}`)
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30)
+    document.getElementById('top').style.display = 'block';
+  else
+    document.getElementById('top').style.display = 'none';
+};
